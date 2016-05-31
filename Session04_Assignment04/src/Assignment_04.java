247b4e35d248e715f85a0cb4d6d7f4d89a5026af
GeekStatused <geekstatused@gmail.com>
2016-05-31 15:42:58 -0400

    Assignment_04
    Session_04

---
+++ Session04_Assignment04/src/Assignment_04.java
@@ -0,0 +1,15 @@
+
+public class Assignment_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//initializing to str using StringBuffer
+		StringBuffer str = new StringBuffer("This is how you reverse a string");
+		System.out.println(str);
+		System.out.print("This is the string reversed : ");
+		System.out.print(str.reverse());
+	}
+
+}
